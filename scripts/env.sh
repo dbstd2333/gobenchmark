db9d372deabc84b0ed9f38df8271670b496f9ebd
dbstd2333 <591055294@qq.com>
2024-10-04 17:14:45 +0800

    修改env适应2c环境

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 nprocs=$(getconf _NPROCESSORS_ONLN)
-if [ $nprocs -lt 4 ]; then
-  echo "Your environment should have at least 4 processors"
-  exit 1
+if [ $nprocs -lt 2 ]; then
+    echo "Your environment should have at least 2 processors"
+    exit 1
 elif [ $nprocs -gt 20 ]; then
-  nprocs=20
+    nprocs=20
 fi
 
 # GO
 GOEXEC=${GOEXEC:-"go"}
 GOROOT=$GOROOT
 
-scpu=$((nprocs > 16 ? 3 : nprocs / 4 - 1)) # max is 3(4 cpus)
+scpu=1 # 将 scpu 固定为 1
 taskset_less="taskset -c 0-$scpu"
 taskset_more="taskset -c $((scpu + 1))-$((nprocs - 1))"
 
